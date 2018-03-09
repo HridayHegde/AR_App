1746d1cc5c2d65c84bd1a9669bf27866a9d80808
Hriday Hegde <hridayhegde1999@gmail.com>
2018-03-09 12:47:48 +0530

    Initial commit

---
+++ AR App/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject Player;
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Dance()
+    {
+        Player.GetComponent<Animator>().SetTrigger("Dance");
+    }
+}
---
+++ AR App/Assets/Scripts/VirtualButtonControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonControl : MonoBehaviour, IVirtualButtonEventHandler {
+    public GameObject VirtuButton;
+    public GameManager GM;
+    // Use this for initialization
+    void Start()
+    {
+        VirtuButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Pressed!!!!!!!!!");
+        GM.Player.GetComponent<Animator>().SetTrigger("Dance");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("Released");
+    }
+
+   
+}
